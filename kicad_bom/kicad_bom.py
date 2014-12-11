9796170173dffbec14edc1f693f93829315afd15
Peter Polidoro <peterpolidoro@gmail.com>
2014-12-11 14:42:23 -0500

    Generate bom and vendor order csv files.

--- kicad_bom/kicad_bom.py
+++ kicad_bom/kicad_bom.py
@@ -29,9 +29,9 @@ def generate_boms():
         output_dir = os.path.join(os.path.dirname(input_path),'bom')
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        output_path = os.path.join(output_dir,'bom.org')
-        if os.path.exists(output_path):
-            shutil.copyfile(output_path,output_path+'.bck')
+        output_path = os.path.join(output_dir,'bom_pcb.csv')
+        # if os.path.exists(output_path):
+        #     shutil.copyfile(output_path,output_path+'.bck')
         f = open(output_path, 'w')
     except IOError:
         e = "Can't open output file for writing: " + output_path
@@ -42,13 +42,13 @@ def generate_boms():
     # by <configure> block in kicad_netlist_reader.py
     components = net.getInterestingComponents()
 
-    # Output a set of rows as a header providing general information
-    source = os.path.basename(net.getSource())
-    f.write('Source: ' + source + '\n')
-    f.write('Date: ' + str(net.getDate()) + '\n')
+    # Header providing general information
+    # source = os.path.basename(net.getSource())
+    # f.write('Source: ' + source + '\n')
+    # f.write('Date: ' + str(net.getDate()) + '\n')
     # f.write('Tool: ' + str(net.getTool()) + '\n')
     # f.write('Component Count: ' + str(len(components)) + '\n')
-    f.write('\n')
+    # f.write('\n')
 
     compfields = net.gatherComponentFieldUnion(components)
     partfields = net.gatherLibPartFieldUnion()
@@ -59,10 +59,10 @@ def generate_boms():
     columnset = compfields | partfields     # union
 
     # prepend an initial 'hard coded' list and put the enchillada into list 'columns'
-    columns = ['!','Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
+    columns = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
 
     # Create a new csv writer object to use as the output formatter
-    out = csv.writer(f,lineterminator='\n',delimiter='|',quotechar='\"',quoting=csv.QUOTE_MINIMAL )
+    out = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL )
 
     # override csv.writer's writerow() to support utf8 encoding:
     def writerow(acsvwriter,columns):
@@ -78,41 +78,76 @@ def generate_boms():
 
     # Output component information organized by group, aka as collated:
     row = []
-    row.append('')
     for c in columns:
         row.append(c)
-    row.append('')
     writerow(out,row)
-    f.write('|-\n')
 
     item = 0
     for group in grouped:
         del row[:]
-        row.append('')
         refs = ""
 
         # Add the reference of every component in the group and keep a reference
         # to the component so that the other data can be filled in once per group
         for component in group:
             if len(refs) > 0:
-                refs += ","
+                refs += " "
             refs += component.getRef()
             c = component
 
         # Fill in the component groups common data
-        # columns = ['!','Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
+        # columns = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
         item += 1
-        row.append('')
         row.append(item)
         row.append(refs);
         row.append(c.getValue())
         row.append(len(group))
 
-        # from column 5 upwards, use the fieldnames to grab the data
-        for field in columns[5:]:
+        # from column 4 upwards, use the fieldnames to grab the data
+        for field in columns[4:]:
             row.append( net.getGroupField(group, field) );
 
-        row.append('')
         writerow(out,row)
 
     f.close()
+
+    # Create vendor set
+    vendor_set = set()
+    for group in grouped:
+        try:
+            vendor = net.getGroupField(group,'Vendor')
+            vendor_set |= set([vendor])
+        except:
+            pass
+
+    # Make order csv file for each vendor
+    for vendor in vendor_set:
+        # Open a file to write to, if the file cannot be opened output to stdout
+        # instead
+        try:
+            output_dir = os.path.join(os.path.dirname(input_path),'bom')
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            filename = str(vendor) + '_order_pcb.csv'
+            output_path = os.path.join(output_dir,filename)
+            f = open(output_path, 'w')
+            # Create a new csv writer object to use as the output formatter
+            out = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL )
+        except IOError:
+            e = "Can't open output file for writing: " + output_path
+            print(__file__,":",e,sys.stderr)
+            f = sys.stdout
+
+        for group in grouped:
+            del row[:]
+            try:
+                if vendor == net.getGroupField(group,'Vendor'):
+                    row.append(len(group))
+                    row.append(net.getGroupField(group,'PartNumber'))
+            except:
+                pass
+
+            writerow(out,row)
+
+        f.close()
+
