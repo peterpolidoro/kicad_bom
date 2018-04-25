3b72941235b1832a07bad3df57c5fe7f9658f6c2
Peter Polidoro <peterpolidoro@gmail.com>
2018-04-25 16:43:40 -0400

    Completed updates.

--- kicad_bom/__init__.py
+++ kicad_bom/__init__.py
@@ -1,4 +1,4 @@
 '''
 KiCad Python module for generating bill of materials in multiple formats.
 '''
-from .kicad_bom import generate_boms
+from .kicad_bom import save_boms_to_files
--- kicad_bom/kicad_bom.py
+++ kicad_bom/kicad_bom.py
@@ -7,7 +7,12 @@ import csv
 
 
 class KicadBom:
-    self._netlist_ext = '.xml'
+    def __init__(self):
+        self._netlist_ext = '.xml'
+        self._netlist_path = None
+        self._netlist = None
+        self._grouped_components = None
+        self._column_names = None
 
     def find_netlist_path(self,netlist_path=None):
         search_path = None
@@ -27,52 +32,51 @@ class KicadBom:
                     return os.path.join(root,f)
         return None
 
-    def read_netlist(self,netlist_path=None):
-        netlist_path = self.find_netlist_path(netlist_path)
-        if netlist_path is None:
+    def _read_netlist(self,netlist_path=None):
+        self._netlist_path = self.find_netlist_path(netlist_path)
+        if self._netlist_path is None:
             return None
-        print("Processing: {0}".format(netlist_path))
 
         # Generate an instance of a generic netlist and load the netlist tree.
-        netlist = kicad_netlist_reader.netlist(netlist_path)
+        self._netlist = kicad_netlist_reader.netlist(self._netlist_path)
 
         # subset the components to those wanted in the BOM, controlled
         # by <configure> block in kicad_netlist_reader.py
-        components = netlist.getInterestingComponents()
+        components = self._netlist.getInterestingComponents()
 
-        # Header providing general information
-        # source = os.path.basename(net.getSource())
-        # f.write('Source: ' + source + '\n')
-        # f.write('Date: ' + str(net.getDate()) + '\n')
-        # f.write('Tool: ' + str(net.getTool()) + '\n')
-        # f.write('Component Count: ' + str(len(components)) + '\n')
-        # f.write('\n')
-
-        compfields = net.gatherComponentFieldUnion(components)
+        compfields = self._netlist.gatherComponentFieldUnion(components)
         compfields -= set(['PartCount'])
-        partfields = net.gatherLibPartFieldUnion()
+        partfields = self._netlist.gatherLibPartFieldUnion()
         # remove Reference, Value, Datasheet, and Footprint, they will come from 'columns' below
         partfields -= set(['Reference','Value','Datasheet','Footprint','PartCount'])
 
         columnset = compfields | partfields     # union
 
         # prepend an initial 'hard coded' list and put the enchillada into list 'columns'
-        columns = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
+        self._column_names = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
 
         # Get all of the components in groups of matching parts + values
         # (see kicad_netlist_reader.py)
-        grouped = net.groupComponents(components)
+        self._grouped_components = self._netlist.groupComponents(components)
 
+    def get_bom_from_netlist(self,netlist_path=None):
+        if (self._netlist_path is None) or (netlist_path is not None):
+            self._read_netlist(netlist_path)
+            if self._netlist_path is None:
+                return []
 
-        # Output component information organized by group, aka as collated:
+        # Create header row
         row = []
-        for c in columns:
+        for c in self._column_names:
             row.append(c)
-        out.writerow(row)
+
+        # Create bom
+        bom = []
+        bom.append(row)
 
         item = 0
-        for group in grouped:
-            del row[:]
+        for group in self._grouped_components:
+            row = []
             refs = ""
 
             # Add the reference of every component in the group and keep a reference
@@ -84,94 +88,93 @@ class KicadBom:
                 c = component
 
             # Fill in the component groups common data
-            # columns = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
+            # self._column_names = ['Item','Reference(s)','Value','Quantity'] + sorted(list(columnset))
             item += 1
             row.append(item)
             row.append(refs);
             row.append(c.getValue())
             part_count = 1
             try:
-                part_count = int(net.getGroupField(group,'PartCount'))
+                part_count = int(self._netlist.getGroupField(group,'PartCount'))
             except ValueError:
                 pass
             row.append(part_count*len(group))
 
             # from column 4 upwards, use the fieldnames to grab the data
-            for field in columns[4:]:
-                row.append( net.getGroupField(group, field) );
+            for field in self._column_names[4:]:
+                row.append( self._netlist.getGroupField(group, field) );
+            bom.append(row)
 
-            out.writerow(row)
+        return bom
 
-        f.close()
-
-    def generate_boms(self,xml_path=None):
-        # Open a file to write to, if the file cannot be opened output to stdout
-        # instead
-        try:
-            output_dir = os.path.join(os.path.dirname(input_path),'bom')
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-            output_path = os.path.join(output_dir,'bom_pcb.csv')
-            print('Creating: {0}'.format(output_path))
-            f = open(output_path,'w',newline='\n')
-        except IOError:
-            e = "Can't open output file for writing: " + output_path
-            print(__file__,":",e,sys.stderr)
-            f = sys.stdout
-
-        # Create a new csv writer object to use as the output formatter
-        out = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL )
+    def get_vendor_orders_from_netlist(self,netlist_path=None):
+        if (self._netlist_path is None) or (netlist_path is not None):
+            self._read_netlist(netlist_path)
+            if self._netlist_path is None:
+                return []
 
         # Create vendor set
         vendor_set = set()
-        for group in grouped:
+        for group in self._grouped_components:
             try:
-                vendor = net.getGroupField(group,'Vendor')
+                vendor = self._netlist.getGroupField(group,'Vendor')
                 vendor_set |= set([vendor])
             except:
                 pass
 
-        # Make order csv file for each vendor
+        vendor_orders = {}
         for vendor in vendor_set:
-            # Open a file to write to, if the file cannot be opened output to stdout
-            # instead
-            try:
-                output_dir = os.path.join(os.path.dirname(input_path),'bom')
-                if not os.path.exists(output_dir):
-                    os.makedirs(output_dir)
-                filename = str(vendor) + '_order_pcb.csv'
-                print('Creating: {0}'.format(filename))
-                output_path = os.path.join(output_dir,filename)
-                f = open(output_path,'w',newline='\n')
-                # Create a new csv writer object to use as the output formatter
-                out = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL )
-            except IOError:
-                e = "Can't open output file for writing: " + output_path
-                print(__file__,":",e,sys.stderr)
-                f = sys.stdout
-
-            for group in grouped:
+            order = []
+            for group in self._grouped_components:
                 try:
-                    if vendor == net.getGroupField(group,'Vendor'):
+                    if vendor == self._netlist.getGroupField(group,'Vendor'):
                         part_count = 1
                         try:
-                            part_count = int(net.getGroupField(group,'PartCount'))
+                            part_count = int(self._netlist.getGroupField(group,'PartCount'))
                         except ValueError:
                             pass
-                        del row[:]
+                        row = []
                         row.append(part_count*len(group))
-                        row.append(net.getGroupField(group,'PartNumber'))
-                        out.writerow(row)
-                except:
+                        row.append(self._netlist.getGroupField(group,'PartNumber'))
+                        order.append(row)
+                except ValueError:
                     pass
+            vendor_orders[vendor] = order
+        return vendor_orders
+
+    def save_boms_to_files(self,netlist_path=None):
+        if (self._netlist_path is None) or (netlist_path is not None):
+            self._read_netlist(netlist_path)
+            if self._netlist_path is None:
+                return []
+
+        output_dir = os.path.join(os.path.dirname(self._netlist_path),'bom')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        bom = self.get_bom_from_netlist()
+        bom_filename = 'bom_pcb.csv'
+        bom_output_path = os.path.join(output_dir,bom_filename)
+        with open(bom_output_path,'w') as f:
+            bom_writer = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL)
+            for row in bom:
+               bom_writer.writerow(row)
+
+        vendor_orders = self.get_vendor_orders_from_netlist()
+        for vendor in vendor_orders:
+            vendor_order_filename = str(vendor) + '_order_pcb.csv'
+            vendor_order_output_path = os.path.join(output_dir,vendor_order_filename)
+            with open(vendor_order_output_path,'w') as f:
+                vendor_order_writer = csv.writer(f,quotechar='\"',quoting=csv.QUOTE_MINIMAL)
+                vendor_order = vendor_orders[vendor]
+                for row in vendor_order:
+                   vendor_order_writer.writerow(row)
 
 
-            f.close()
-
-def generate_boms():
+def save_boms_to_files():
     kb = KicadBom()
-    kb.generate_boms()
+    kb.save_boms_to_files()
 
 # -----------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    generate_boms()
+    save_boms_to_files()
